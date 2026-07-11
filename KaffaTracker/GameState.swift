648b6da8c8e5dfc1fb46e8614d8f563259225735
Syed Rahaman <Syed.Rahaman@cshs.org>
2026-07-11 22:06:45 +0530

    GameState enum

---
+++ KaffaTracker/GameState.swift
@@ -0,0 +1,14 @@
+//
+//  GameState.swift
+//  KaffaTracker
+//
+//  Created by Syed Rahaman on 21/06/26.
+//
+
+import Foundation
+
+enum GameState {
+    case setup
+    case playing
+    case gameOver
+}
